9cc659a5664839af28fcc54c98e4687532b77f09
realth000 <a598919335@gmail.com>
2023-01-18 16:36:03 +0800

    feat(crypto): Move and add more hash functions
    Move and add more hash functions:
    * Move hash functions to separate hash package inside crypto package.
    * Add SHA-3 related functions.
    * Add Hash() and HashString() function to get byte slice as result.

--- crypto/hash/hash.go
+++ crypto/hash/hash.go
@@ -5,6 +5,23 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/sha512"
+	"golang.org/x/crypto/sha3"
+)
+
+type SumType = int8
+
+const (
+	SumMD5 SumType = iota
+	SumSHA1
+	SumSHA224
+	SumSHA256
+	SumSHA384
+	SumSHA512
+	SumSHA512_256
+	SumSHA3_224
+	SumSHA3_256
+	SumSHA3_384
+	SumSHA3_512
 )
 
 func MD5(data []byte) [16]byte {
@@ -23,6 +40,14 @@ func SHA1String(data string) [20]byte {
 	return SHA1([]byte(data))
 }
 
+func SHA224(data []byte) [28]byte {
+	return sha256.Sum224(data)
+}
+
+func SHA224String(data string) [28]byte {
+	return SHA224([]byte(data))
+}
+
 func SHA256(data []byte) [32]byte {
 	return sha256.Sum256(data)
 }
@@ -31,10 +56,146 @@ func SHA256String(data string) [32]byte {
 	return SHA256([]byte(data))
 }
 
-func SHA512256(data []byte) [32]byte {
+func SHA384(data []byte) [48]byte {
+	return sha512.Sum384(data)
+}
+
+func SHA384String(data string) [48]byte {
+	return SHA384([]byte(data))
+}
+
+func SHA512(data []byte) [64]byte {
+	return sha512.Sum512(data)
+}
+
+func SHA512String(data string) [64]byte {
+	return SHA512([]byte(data))
+}
+
+func SHA512_224(data []byte) [28]byte {
+	return sha3.Sum224(data)
+}
+
+func SHA512_224String(data string) [28]byte {
+	return SHA512_224([]byte(data))
+}
+
+func SHA512_256(data []byte) [32]byte {
 	return sha512.Sum512_256(data)
 }
 
-func SHA512256String(data string) [32]byte {
-	return SHA512256([]byte(data))
+func SHA512_256String(data string) [32]byte {
+	return SHA512_256([]byte(data))
+}
+
+func SHA3_224(data []byte) [28]byte {
+	return sha3.Sum224(data)
+}
+
+func SHA3_224String(data string) [28]byte {
+	return SHA3_224([]byte(data))
+}
+
+func SHA3_256(data []byte) [32]byte {
+	return sha3.Sum256(data)
+}
+
+func SHA3_256String(data string) [32]byte {
+	return SHA3_256([]byte(data))
+}
+
+func SHA3_384(data []byte) [48]byte {
+	return sha3.Sum384(data)
+}
+
+func SHA3_384String(data string) [48]byte {
+	return SHA3_384([]byte(data))
+}
+
+func SHA3_512(data []byte) [64]byte {
+	return sha3.Sum512(data)
+}
+
+func SHA3_512String(data string) [64]byte {
+	return sha3.Sum512([]byte(data))
+}
+
+func Hash(sumType SumType, data []byte) []byte {
+	switch sumType {
+	case SumMD5:
+		v := MD5(data)
+		return v[:]
+	case SumSHA1:
+		v := SHA1(data)
+		return v[:]
+	case SumSHA224:
+		v := SHA224(data)
+		return v[:]
+	case SumSHA256:
+		v := SHA256(data)
+		return v[:]
+	case SumSHA384:
+		v := SHA384(data)
+		return v[:]
+	case SumSHA512:
+		v := SHA512(data)
+		return v[:]
+	case SumSHA512_256:
+		v := SHA512_256(data)
+		return v[:]
+	case SumSHA3_224:
+		v := SHA3_224(data)
+		return v[:]
+	case SumSHA3_256:
+		v := SHA3_256(data)
+		return v[:]
+	case SumSHA3_384:
+		v := SHA3_384(data)
+		return v[:]
+	case SumSHA3_512:
+		v := SHA3_512(data)
+		return v[:]
+	default:
+		panic("invalid sum hash type")
+	}
+}
+
+func HashString(sumType SumType, data string) []byte {
+	switch sumType {
+	case SumMD5:
+		v := MD5String(data)
+		return v[:]
+	case SumSHA1:
+		v := SHA1String(data)
+		return v[:]
+	case SumSHA224:
+		v := SHA224String(data)
+		return v[:]
+	case SumSHA256:
+		v := SHA256String(data)
+		return v[:]
+	case SumSHA384:
+		v := SHA384String(data)
+		return v[:]
+	case SumSHA512:
+		v := SHA512String(data)
+		return v[:]
+	case SumSHA512_256:
+		v := SHA512_256String(data)
+		return v[:]
+	case SumSHA3_224:
+		v := SHA3_224String(data)
+		return v[:]
+	case SumSHA3_256:
+		v := SHA3_256String(data)
+		return v[:]
+	case SumSHA3_384:
+		v := SHA3_384String(data)
+		return v[:]
+	case SumSHA3_512:
+		v := SHA3_512String(data)
+		return v[:]
+	default:
+		panic("invalid sum hash type")
+	}
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,12 @@ go 1.19
 
 require (
 	github.com/PuerkitoBio/goquery v1.8.0
-	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8
+	golang.org/x/net v0.5.0
 	golang.org/x/text v0.6.0
 )
 
-require github.com/andybalholm/cascadia v1.3.1 // indirect
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/crypto v0.5.0 // indirect
+	golang.org/x/sys v0.4.0 // indirect
+)
